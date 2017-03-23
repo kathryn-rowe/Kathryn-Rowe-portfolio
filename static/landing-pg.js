0972a9c78085cde46bdedcb9d92df80fff1f5333
Kathryn Rowe <kjrowe06@gmail.com>
2017-03-22 17:20:15 -0700

    added bootstrap modals

--- static/landing-pg.js
+++ static/landing-pg.js
@@ -1,14 +1,15 @@
 "use strict"
 
-
+// Mousover and turn picture to full-color
 function eliminateFilter(evt) {
     $(this).css('filter', 'grayscale(0%)');
 }
 
+$('img.picture').on('mouseover', eliminateFilter);
+
+// Mouseout and reset grayscale
 function resetFilter(evt) {
     $(this).css('filter', 'grayscale(90%)');
 }
-
-$('img.picture').on('mouseover', eliminateFilter);
 
 $('img.picture').on('mouseout', resetFilter);
---
+++ static/modal.js
@@ -0,0 +1,23 @@
+"use strict"
+console.log('hello1');
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal
+var img = document.getElementsByClassName('myImg');
+var modalImg = document.getElementById("img01");
+
+$('.myImg').on('click', function(){
+    console.log('hello');
+    modal.style.display = "block";
+    modalImg.src = this.src;
+})
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
