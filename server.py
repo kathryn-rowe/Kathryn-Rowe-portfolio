e34f588a77422ffc1679039021c06a2701fe8fa4
Kathryn Rowe <kjrowe06@gmail.com>
2017-03-24 10:08:06 -0700

    Debugging heroku

--- server.py
+++ server.py
@@ -1,32 +1,25 @@
 """Bird abundance by observation."""
 import os
 
-from jinja2 import StrictUndefined
-
 from flask import (Flask,
-                   render_template,
-                   g)
+                   render_template)
 
 # from flask_debugtoolbar import DebugToolbarExtension
-
-import secret_key
-
-# from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "abcdef")
 
-JS_TESTING_MODE = False
+# JS_TESTING_MODE = False
 
 # If you use an undefined variable in Jinja2, it raises an error.
-app.jinja_env.undefined = StrictUndefined
+# app.jinja_env.undefined = StrictUndefined
 
-app.secret_key = secret_key.flask_secret_key
+# app.secret_key = secret_key.flask_secret_key
 
 
-@app.before_request
-def add_tests():
-    g.jasmine_tests = JS_TESTING_MODE
+# @app.before_request
+# def add_tests():
+#     g.jasmine_tests = JS_TESTING_MODE
 
 
 @app.route('/')
@@ -114,15 +107,15 @@ def error():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
-    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+    # app.debug = True
+    # app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
 
-    import sys
-    if sys.argv[-1] == "jstest":
-        JS_TESTING_MODE = True
+    # import sys
+    # if sys.argv[-1] == "jstest":
+    #     JS_TESTING_MODE = True
 
     DEBUG = "NO_DEBUG" not in os.environ
     PORT = int(os.environ.get("PORT", 5000))
